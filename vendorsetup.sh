0ed6ea03dca2f0f95665570bdd17ff2de7effac8
Paul Keith <javelinanddart@gmail.com>
2015-08-26 20:20:24 -0500

    quincydcm: Make it CM
    Change-Id: Id0ea3d4a0a32e06c0c76f9a4e3ee705740cdd708
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo candy5_quincydcm-user
-add_lunch_combo candy5_quincydcm-userdebug
-add_lunch_combo candy5_quincydcm-eng
+add_lunch_combo cm_quincydcm-user
+add_lunch_combo cm_quincydcm-userdebug
+add_lunch_combo cm_quincydcm-eng
